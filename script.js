26a411c0644fc96d20663fc7163c7fa728b05f02
zahaaabdulmatheen-jpg <zahaaabdulmatheen@gmail.com>
2026-05-24 00:18:57 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-function scrollToPapers() {
-  document.getElementById("papers").scrollIntoView({ behavior: "smooth" });
-}
-
 function showPapers() {
-  const subject = document.getElementById("subject").value;
-  const year = document.getElementById("year").value;
 
   document.getElementById("results").innerHTML = `
-    <h3>${subject} - ${year}</h3>
-    <p>📄 Paper 1 (example link)</p>
+
+    <h2>Mathematics Papers</h2>
+
+    <ul>
+
+      <li>
+        <a href="papers/IGCSE/Mathematics/2024/October-November 42.pdf" target="_blank">
+          October-November 42
+        </a>
+      </li>
+
+      <li>
+        <a href="papers/IGCSE/Mathematics/2024/October-November 22.pdf" target="_blank">
+          October-November 22
+        </a>
+      </li>
+
+      <li>
+        <a href="papers/IGCSE/Mathematics/2024/May-June 42.pdf" target="_blank">
+          May-June 42
+        </a>
+      </li>
+
+      <li>
+        <a href="papers/IGCSE/Mathematics/2024/May-June 22.pdf" target="_blank">
+          May-June 22
+        </a>
+      </li>
+
+    </ul>
+
   `;
 }
